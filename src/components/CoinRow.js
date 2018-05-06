fcc4afd80ddf9f7682d8521d2d05cfdcdef03a08
Boonsuen Oh <ohboonsuen123@gmail.com>
2018-05-06 23:27:14 +0800

    sort feature finished | 1

--- src/components/CoinRow.js
+++ src/components/CoinRow.js
@@ -5,21 +5,14 @@ const MyFacebookLoader = () => <Facebook />
 
 const MyLoader = props => (
 	<ContentLoader
-		height={160}
+		height={50}
 		width={400}
 		speed={1.5}
 		primaryColor="#f3f3f3"
 		secondaryColor="#ecebeb"
 		{...props}
 	>
-		<circle cx="35" cy="35" r="8" />
-		<rect x="50" y="30" rx="5" ry="5" width="220" height="10" />
-		<circle cx="35" cy="65" r="8" />
-		<rect x="50" y="60" rx="5" ry="5" width="220" height="10" />
-		<circle cx="35" cy="95" r="8" />
-		<rect x="50" y="90" rx="5" ry="5" width="220" height="10" />
-		<circle cx="35" cy="125" r="8" />
-		<rect x="50" y="120" rx="5" ry="5" width="220" height="10" />
+		<rect x="0" y="13" rx="5" ry="5" width="150" height="25" /> 
 	</ContentLoader>
 )
 
@@ -44,27 +37,14 @@ const CoinRow = (props) => {
         </tr>
       )
       :
-      <tr><td>no data yet</td></tr>
+      <tr>
+        <td><MyLoader /></td>
+        <td><MyLoader /></td>
+        <td><MyLoader /></td>
+      </tr>
     }
     </React.Fragment>
   );
 };
-
-// const CoinRow = (props) => {
-//   let className = 'pc';
-//   if (props.priceChange > 0) {
-//     className += ' pc--green';
-//   } else if (props.priceChange < 0) {
-//     className += ' pc--red';
-//   }
-//   const priceSats = Math.floor(props.priceSats * 100000000);
-//   return (
-//     <tr>
-//       <td>{props.name} ({props.symbol})</td>
-//       <td>${props.name === 'Bitcoin' ? props.priceUsd : `${props.priceUsd} / ${priceSats}`}</td>
-//       <td className={className}>{props.priceChange}</td>
-//     </tr>
-//   );
-// };
 
 export default CoinRow;
--- src/components/WatchPriceTable.js
+++ src/components/WatchPriceTable.js
@@ -29,6 +29,27 @@ export default class WatchPriceTable extends React.Component {
       }));
     }
   }
+  handleSortChange = (e) => {
+    if (e.target.className.baseVal === 'sorter__up') {
+      const sortedCoinsData = [...this.state.coinsData];
+      sortedCoinsData.map((obj) => {
+        obj.percent_change_24h = Number(obj.percent_change_24h);
+      });
+      sortedCoinsData.sort((a, b) => b.percent_change_24h - a.percent_change_24h);
+      this.setState(() => ({
+        coinsData: sortedCoinsData
+      }));
+    } else if (e.target.className.baseVal === 'sorter__down') {
+      const sortedCoinsData = [...this.state.coinsData];
+      sortedCoinsData.map((obj) => {
+        obj.percent_change_24h = Number(obj.percent_change_24h);
+      });
+      sortedCoinsData.sort((a, b) => a.percent_change_24h - b.percent_change_24h);
+      this.setState(() => ({
+        coinsData: sortedCoinsData
+      }));
+    };
+  };
   render() {
     return (
       <table className="price-table">
@@ -36,7 +57,22 @@ export default class WatchPriceTable extends React.Component {
           <tr className="row-head">
             <th className="name">Name</th>
             <th className="price">Price</th>
-            <th className="change">Change</th>
+            <th className="change">
+              Change
+              <svg className="sorter" version="1.1" x="0px" y="0px"
+                viewBox="0 0 194.2 348.7" space="preserve">
+                <path
+                  className="sorter__up"
+                  onClick={this.handleSortChange}
+                  d="M86.2,5.6l-83.8,118c-6.3,8.8,0,21,10.9,21h167.5c10.8,0,17.1-12.2,10.9-21L108,5.6
+                  C102.7-1.9,91.6-1.9,86.2,5.6z"/>
+                <path
+                  className="sorter__down"
+                  onClick={this.handleSortChange}
+                  d="M108,343.1l83.8-118c6.3-8.8,0-21-10.9-21H13.3c-10.8,0-17.1,12.2-10.9,21l83.8,118
+                  C91.6,350.5,102.7,350.5,108,343.1z"/>
+              </svg>
+            </th>
           </tr>
         </thead>
         <tbody>
@@ -49,6 +85,7 @@ export default class WatchPriceTable extends React.Component {
               priceUsd={coin.price_usd}
               priceSats={coin.price_btc}
               priceChange={coin.percent_change_24h}
+              coinsDataLoaded={!!this.state.coinsData}
             />
           ))
         }
