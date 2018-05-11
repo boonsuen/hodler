2a721a0347c58670d7e8d09579fd210b2e766f66
Boonsuen Oh <ohboonsuen123@gmail.com>
2018-05-11 14:37:05 +0800

    content loader finished | 2

--- src/components/CoinRow.js
+++ src/components/CoinRow.js
@@ -3,18 +3,44 @@ import ContentLoader, { Facebook } from 'react-content-loader'
 
 const MyFacebookLoader = () => <Facebook />
 
-const MyLoader = props => (
+const NameLoader = props => (
 	<ContentLoader
 		height={50}
-		width={400}
+		width={253}
 		speed={1.5}
 		primaryColor="#f3f3f3"
 		secondaryColor="#ecebeb"
 		{...props}
 	>
-		<rect x="0" y="13" rx="5" ry="5" width="150" height="25" /> 
+		<rect x="0" y="12.5" rx="5" ry="5" width="110" height="25" />
 	</ContentLoader>
-)
+);
+
+const PriceLoader = props => (
+  <ContentLoader
+    height={43}
+		width={225}
+		speed={1.5}
+		primaryColor="#f3f3f3"
+		secondaryColor="#ecebeb"
+		{...props}
+	>
+		<rect x="0" y="9" rx="5" ry="5" width="150" height="25" />
+	</ContentLoader>
+);
+
+const ChangeLoader = props => (
+	<ContentLoader
+		height={38}
+		width={149}
+		speed={1.5}
+		primaryColor="#f3f3f3"
+		secondaryColor="#ecebeb"
+		{...props}
+	>
+		<rect x="0" y="6" rx="5" ry="5" width="60" height="25" />
+	</ContentLoader>
+);
 
 const CoinRow = (props) => {
   let className = 'pc';
@@ -37,10 +63,10 @@ const CoinRow = (props) => {
         </tr>
       )
       :
-      <tr>
-        <td><MyLoader /></td>
-        <td><MyLoader /></td>
-        <td><MyLoader /></td>
+      <tr className="svg-tr">
+        <td><NameLoader /></td>
+        <td><PriceLoader /></td>
+        <td><ChangeLoader /></td>
       </tr>
     }
     </React.Fragment>
