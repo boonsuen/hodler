f62777639635ec2eafca6f64366ef94c968a0a5b
Boonsuen Oh <ohboonsuen123@gmail.com>
2018-11-03 14:12:22 +0800

    Delete some stuff

--- .storybook/config.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { configure, storiesOf } from '@storybook/react';
-
-function loadStories() {
-  require('../stories/index.js');
-}
-
-configure(loadStories, module);
--- src/app.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import HodlerApp from './components/HodlerApp';
-
-ReactDOM.render(<HodlerApp />, document.getElementById('app'));
