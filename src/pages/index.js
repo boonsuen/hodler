c3b072bcd9b9006569408689b97b3a35c082fd58
Boonsuen Oh <ohboonsuen123@gmail.com>
2018-11-04 14:46:06 +0800

    Modified pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,11 @@
-import HodlerApp from '../components/HodlerApp';
+import React from 'react';
+import Layout from '../components/Layout';
+import PriceTable from '../components/PriceTable';
 
-export default HodlerApp;
+const IndexPage = () => (
+  <Layout>
+    <PriceTable view="hold" />
+  </Layout>
+);
+
+export default IndexPage;
