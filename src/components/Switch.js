c912f36f28fbabe8f09b68c9fd8ac5c961fa8f53
Boonsuen Oh <ohboonsuen123@gmail.com>
2018-11-04 14:45:48 +0800

    New components

--- src/components/Switch.js
+++ src/components/Switch.js
@@ -1,55 +1,73 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'gatsby';
+import styled from 'styled-components';
 
-export default class Switch extends React.Component {
-  state = {
-    classNames: {
-      hold: 'active hold',
-      watch: 'inactive watch'
-    }
-  };
-  componentDidMount() {
-    if (this.props.view === 'hold') {
-      this.setState(() => ({
-        classNames: {
-          hold: 'active hold',
-          watch: 'inactive watch'
-        }
-      }))
-    } else if (this.props.view === 'watch') {
-      this.setState(() => ({
-        classNames: {
-          hold: 'inactive hold',
-          watch: 'active watch'
-        }
-      }));
-    }
+const Switch = styled.div`
+  margin: 0 auto 30px auto;
+  display: flex;
+  width: 199px;
+  justify-content: center;
+  
+  a {
+    height: 45px;
+    cursor: pointer;
+    text-align: center;
+    line-height: 45px;
+    width: 100px;
+    box-sizing: border-box;
   }
-  handleSwitch = (e) => {
-    const classNames = e.target.className.split(' ');
-    const notMatched = !classNames.find(elem => elem === this.props.view);
-    if (this.props.view === 'hold' && notMatched) {
-      this.setState(() => ({
-        classNames: {
-          hold: 'inactive hold',
-          watch: 'active watch'
-        }
-      }));
-    } else if (this.props.view === 'watch' && notMatched) {
-      this.setState(() => ({
-        classNames: {
-          hold: 'active hold',
-          watch: 'inactive watch'
-        }
-      }));
-    }
-  };
+`;
+
+const StyledLink = styled(({ isActive, children, ...rest }) => (
+  <Link {...rest}>{children}</Link>
+))`
+  background-color: ${props => props.isActive ? "#EE732F" : "none"};
+  color: ${props => props.isActive ? "#fff" : "#EE732F"};
+  border: 1px solid #EE732F;
+  transition: all .2s;
+
+  &:hover {
+    background-color: ${props => !props.isActive && "rgba(255,107,0,0.1)"};
+    color: ${props => !props.isActive && "#EE732F"};
+  }
+`;
+
+class StyledSwitch extends React.Component {
+  getSelectedLink = () => {
+    if (typeof window !== `undefined`) {
+      if (window.location.pathname === '/') {
+        return 'hold'
+      } else if (window.location.pathname === '/watch' || window.location.pathname === '/watch/') {
+        return 'watch'
+      } 
+    }    
+  }
+  state = {
+    selectedLink: this.getSelectedLink()
+  }
+  isActive = (selectedLink) => this.state.selectedLink === selectedLink;
+  handleSwitch = (selectedLink) => {
+    this.setState({selectedLink});
+  }
   render() {
     return (
-      <div className="switch" onClick={this.handleSwitch}>
-        <NavLink activeClassName="" className={this.state.classNames.hold} to="/" exact={true}>Holding</NavLink>
-        <NavLink activeClassName="" className={this.state.classNames.watch} to="/watch">Watching</NavLink>
-      </div>
+      <Switch>
+        <StyledLink 
+          isActive={this.isActive('hold')}       
+          onClick={() => {
+            this.handleSwitch('hold')
+          }}  
+          to="/">Holding</StyledLink>
+        <StyledLink 
+          isActive={this.isActive('watch')}
+          onClick={() => {
+            console.log('yo')
+            this.handleSwitch('watch')
+          }} 
+          to="/watch">Watching</StyledLink>
+      </Switch>
     );
   }
 }
+
+export default StyledSwitch;
