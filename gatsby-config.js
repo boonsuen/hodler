8e4efd0578453f6a29e512321892a23e95b94b1d
Boonsuen Oh <ohboonsuen123@gmail.com>
2018-11-04 15:13:14 +0800

    Test gatsby-plugin-layout

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,12 @@
 module.exports = {
   plugins: [
     'gatsby-plugin-styled-components',
-  ]
+    {
+      resolve: `gatsby-plugin-layout`,
+      options: {
+        component: require.resolve(`./src/components/Layout.js`)
+      }
+  }
+  ],
+  pathPrefix: `/hodler-react`,
 }
