c70ac7d53a760b22e6b773362de81a580219b99a
Boonsuen Oh <ohboonsuen123@gmail.com>
2018-11-12 23:14:57 +0800

    Class to function

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import PriceTable from '../components/PriceTable';
 
-const Home = () => {
-  return (
-    <PriceTable 
-      coinsToRender={[1, 2092, 2132, 1697]}
-    />
-  )
-};
+const Home = () => (
+  <PriceTable 
+    coinsToRender={[1, 2092, 2132, 1697]}
+  />
+);
 
 export default Home;
