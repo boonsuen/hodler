105f821167d404886cffc14118f03c61c7a08990
Boonsuen Oh <ohboonsuen123@gmail.com>
2018-11-14 21:30:58 +0800

    Get the sorting functionality to work

--- src/components/PriceTable.js
+++ src/components/PriceTable.js
@@ -36,8 +36,16 @@ class PriceTable extends React.Component {
       });
     }
   }
-  handleSortChange = (e) => {
-    console.log(this.getCoinsData());
+  handleSortChange = (rule) => {
+    if (rule === 'up') {
+      this.setState(state => ({
+        coinsData: state.coinsData.sort((a, b) => b.quote.USD.percent_change_24h - a.quote.USD.percent_change_24h)
+      }));
+    } else if (rule === 'down') {
+      this.setState(state => ({
+        coinsData: state.coinsData.sort((a, b) => a.quote.USD.percent_change_24h - b.quote.USD.percent_change_24h)
+      }));
+    };
   };
   render() {
     const { coinsToRender, loaded } = this.props;
@@ -53,12 +61,12 @@ class PriceTable extends React.Component {
                 viewBox="0 0 194.2 348.7" space="preserve">
                 <path
                   className="sorter__up"
-                  onClick={this.handleSortChange}
+                  onClick={() => {this.handleSortChange('up')}}
                   d="M86.2,5.6l-83.8,118c-6.3,8.8,0,21,10.9,21h167.5c10.8,0,17.1-12.2,10.9-21L108,5.6
                   C102.7-1.9,91.6-1.9,86.2,5.6z"/>
                 <path
                   className="sorter__down"
-                  onClick={this.handleSortChange}
+                  onClick={() => {this.handleSortChange('down')}}
                   d="M108,343.1l83.8-118c6.3-8.8,0-21-10.9-21H13.3c-10.8,0-17.1,12.2-10.9,21l83.8,118
                   C91.6,350.5,102.7,350.5,108,343.1z"/>
               </svg>
