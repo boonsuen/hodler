6339f83d8f910c74e452e204e2195e5e5b0604c8
Boonsuen Oh <ohboonsuen123@gmail.com>
2018-11-14 20:40:56 +0800

    Refactor the @font-face rules

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 import Header from './components/Header';
 import Switch from './components/Switch';
 
-import GlobalStyle from './GlobalStyle.css';
+import GlobalStyle, { fontFaceRules } from './GlobalStyle.css';
 import img_favicon from './assets/img/favicon.png';
 
 export const CoinsDataContext = React.createContext({  
@@ -38,35 +38,7 @@ export default class App extends React.Component {
           <title>Hodler</title>
           <link rel="icon" href={img_favicon} />
           <style>
-          {`
-            @font-face {
-              font-family: "Avenir Next";
-              src: url('/fonts/AvenirNextLTPro-Regular-subset.woff2') format('woff2'),
-                  url('/fonts/AvenirNextLTPro-Regular-subset.woff') format('woff');
-              font-weight: 400;
-            }
-
-            @font-face {
-              font-family: "Avenir Next";
-              src: url('/fonts/AvenirNextLTPro-Medium-subset.woff2') format('woff2'),
-                  url('/fonts/AvenirNextLTPro-Medium-subset.woff') format('woff');
-              font-weight: 500;
-            }
-
-            @font-face {
-              font-family: "Avenir Next";
-              src: url('/fonts/AvenirNextLTPro-Demi-subset.woff2') format('woff2'),
-                  url('/fonts/AvenirNextLTPro-Demi-subset.woff') format('woff');
-              font-weight: 600;
-            }
-
-            @font-face {
-              font-family: "Avenir Next";
-              src: url('/fonts/AvenirNextLTPro-Bold-subset.woff2') format('woff2'),
-                  url('/fonts/AvenirNextLTPro-Bold-subset.woff') format('woff');
-              font-weight: 700;
-            }
-          `}
+            {fontFaceRules}
           </style>
         </Head>
         <GlobalStyle />        
--- src/GlobalStyle.css.js
+++ src/GlobalStyle.css.js
@@ -12,6 +12,36 @@ import AvenirNextDemiSubsetWoff from './assets/fonts/AvenirNextLTPro-Demi-subset
 import AvenirNextBoldSubsetWoff2 from './assets/fonts/AvenirNextLTPro-Bold-subset.woff2';
 import AvenirNextBoldSubsetWoff from './assets/fonts/AvenirNextLTPro-Bold-subset.woff';
 
+export const fontFaceRules = `
+  @font-face {
+    font-family: "Avenir Next";
+    src: url(${AvenirNextRegularSubsetWoff2}) format('woff2'),
+        url(${AvenirNextRegularSubsetWoff}) format('woff');
+    font-weight: 400;
+  }
+
+  @font-face {
+    font-family: "Avenir Next";
+    src: url(${AvenirNextMediumSubsetWoff2}) format('woff2'),
+        url(${AvenirNextMediumSubsetWoff}) format('woff');
+    font-weight: 500;
+  }
+
+  @font-face {
+    font-family: "Avenir Next";
+    src: url(${AvenirNextDemiSubsetWoff2}) format('woff2'),
+        url(${AvenirNextDemiSubsetWoff}) format('woff');
+    font-weight: 600;
+  }
+
+  @font-face {
+    font-family: "Avenir Next";
+    src: url(${AvenirNextBoldSubsetWoff2}) format('woff2'),
+        url(${AvenirNextBoldSubsetWoff}) format('woff');
+    font-weight: 700;
+  }
+`;
+
 const GlobalStyle = createGlobalStyle`
   body {
     font-family: "Avenir Next", -apple-system, BlinkMacSystemFont, Roboto, Helvetica, sans-serif;
