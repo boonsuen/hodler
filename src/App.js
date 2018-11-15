9c5d9383da5d9b5ebff172d72cd3846c43c4ca15
Boonsuen Oh <ohboonsuen123@gmail.com>
2018-11-15 17:45:35 +0800

    Updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Head, Route, cleanPath } from 'react-static';
+import { Router, Head } from 'react-static';
 import Routes from 'react-static-routes';
 import axios from 'axios';
 
