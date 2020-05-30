bd8c44aab789d043da99318b1340a29d45deebf2
Boonsuen Oh <boonsuenoh@gmail.com>
2020-05-30 23:39:01 +0800

    add exportPathMap

--- next.config.js
+++ next.config.js
@@ -26,4 +26,13 @@ module.exports = {
 
     return config
   },
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      '/watch': { page: '/watch' }
+    }
+  },
 }
